92dbe361c8553167ffb6f532377e33312f2091a8
Nihal Gupta <nihalgupta140@gmail.com>
2021-12-02 22:46:00 +0530

    Add solution for problem `A - Long Comparison` Link: https://codeforces.com/contest/1613/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1613/submission/137821645

---
+++ codeforces/1613/A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        long long x1,x2;
+        int p1,p2;
+        cin>>x1>>p1>>x2>>p2;
+        int mn = min(p1,p2);
+        p1 -= mn;
+        p2 -= mn;
+        if(p1>=7) cout << '>' << '\n';
+        else if(p2>=7) cout << '<' << '\n';
+        else
+        {
+            for (int i=0; i<p1; ++i) x1 *=10;
+            for(int i=0; i<p2; ++i) x2 *=10;
+            if(x1<x2) cout << "<" << '\n';
+            else if(x1>x2) cout << ">" << '\n';
+            else cout << "=" << '\n';
+        }
+    }  
+    return 0;
+}
