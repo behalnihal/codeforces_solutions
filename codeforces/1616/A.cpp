a912f0487149343c80a28ad8661860dc6c283840
Nihal Gupta <nihalgupta140@gmail.com>
2021-12-29 21:59:00 +0530

    Add solution for problem `A - Integer Diversity` Link: https://codeforces.com/contest/1616/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1616/submission/141093181

--- codeforces/1616/A.cpp
+++ codeforces/1616/A.cpp
@@ -1,15 +1,6 @@
 #include"bits/stdc++.h"
 using namespace std;
 
-void countFreq(int arr[], int n)
-{
-    unordered_map<int, int> mp;
- 
-    // Traverse through array elements and
-    // count frequencies
-    for (int i = 0; i < n; i++)
-        mp[arr[i]]++;
-}
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -27,16 +18,15 @@ int main()
         for(auto &v : a) cin>>v;
         unordered_map<int,int> mp;
         for(int i=0; i<n; i++){
-        	if(a[i]<0) mp[-1 *a[i]]++;
-        	else mp[a[i]]++;
+            if(a[i]<0) mp[-1 * a[i]]++;
+            else mp[a[i]]++;
         }
         int ans = 0;
         for(auto x : mp)
         {
             if(x.first==0) ans+=1;
-            else{
-                x.second>=2 ? ans+=2 : ans +=1;
-            }
+            else if(x.second>1) ans +=2;
+            else if(x.second==1) ans++;
         }
         cout << ans << '\n';
 
