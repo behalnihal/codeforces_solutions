b217227e20983ca4d4516c46a5e4c538995a69a6
Nihal Gupta <nihalgupta140@gmail.com>
2022-01-01 23:39:00 +0530

    Add solution for problem `B - Xenia and Ringroad` Link: https://codeforces.com/contest/339/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/339/submission/141340873

--- codeforces/339/B.cpp
+++ codeforces/339/B.cpp
@@ -1,31 +1,29 @@
 #include"bits/stdc++.h"
 using namespace std;
 
-const int MOD = 1e9 + 7;
-typedef long long ll;
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define rep(i,a,b) for(int i=0; i<b; i++)
+#define ll long long
+
 
 int main()
-{	
-	ios_base::sync_with_stdio(0);
+{
+	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	ll n,m;
-	cin>>n>>m;
-	ll ans=0, cur=0;
-	rep(i,0,m){
-		ll x;
-		cin>>x;
-		--x;
-		if(x<cur) ans += n-cur+x;
-		else ans += x-cur;
-		cur = x;
-	}
-	cout << ans;
+	ll n, m, cur = 0;
+    ll res = 0;
+    cin >> n >> m;
+    for(int i = 0; i < m; i++)
+    {
+        ll x;
+        cin >> x;
+        --x;
+        if (x < cur)
+            res += n - cur + x;
+        else
+            res += x - cur;
+        cur = x;
+    }
+    cout << res;
 
 	return 0;
 }
