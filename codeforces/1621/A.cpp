14a182312f9d23bb8ab8b1cf701ca89d738d864a
Nihal Gupta <nihalgupta140@gmail.com>
2022-01-03 21:25:00 +0530

    Add solution for problem `A - Stable Arrangement of Rooks` Link: https://codeforces.com/contest/1621/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1621/submission/141543454

--- codeforces/1621/A.cpp
+++ codeforces/1621/A.cpp
@@ -3,33 +3,34 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 
-    int t;
-    cin>>t;
+	int t;
+	cin>>t;
 
-    while(t--)
-    {
-        int n,k;
-        cin>>n>>k;
-        
-        if(k > ceil((double)n/2)) cout << -1 << '\n';
-        else{
-            for(int i=0; i<n; i++)
-            {
-                for(int j=0; j<n; j++)
-                {
-                    if(i==j && i%2==0 && k>0){
-                        cout << 'R';
-                        k--;
-                    }
-                    else cout << '.';
-                }
-                cout << '\n';
-            }
-        }
-    }
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		int a[n][n];
+		if(n%2==0 && k>n/2) cout << -1 << '\n';
+		else if(n%2!=0 && k>n/2+1) cout << -1 << '\n';
+		else{
+			for(int i=0; i<n; i++)
+			{
+				for(int j=0; j<n; j++)
+				{
+					if(i==j && i%2==0 && k>0){
+						cout << 'R';
+						k--;
+					}
+					else cout << '.';
+				}
+				cout << '\n';
+			}
+		}
+	}
 
-    return 0;
+	return 0;
 }
