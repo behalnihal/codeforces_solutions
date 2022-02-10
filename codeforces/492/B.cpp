9ad0754a088cbba496aa87735ca599ee66f84b3d
Nihal Gupta <nihalgupta140@gmail.com>
2022-02-10 15:18:00 +0530

    Add solution for problem `B - Vanya and Lanterns` Link: https://codeforces.com/contest/492/problem/B Tags: binary search, implementation, math, sortings, *1200 Ref: https://codeforces.com/contest/492/submission/145834133

---
+++ codeforces/492/B.cpp
@@ -0,0 +1,43 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+const int32_t MOD = 1e9 + 7;
+typedef long long ll;
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(s) s.begin(),s.end()
+#define rep(i,a,b) for(int i=a; i<b; i++)
+ll expo(ll a,ll b,ll m){a%=m;ll res=1;while(b){if(b&1)res=(a*res)%m;a=(a*a)%m;b>>=1;}return res;}
+const int N = 1e5 + 10;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n,l;
+	cin >> n >> l;
+
+	vector<int> a(n);
+	rep(i,0,n){
+		cin >> a[i];
+	}
+	sort(all(a));
+	// for(auto x : a) cout << x << ' ';
+		// cout << '\n';
+	int maxd = -1;
+	rep(i,0,n-1){
+		if(maxd < (a[i+1] - a[i])){
+			maxd = a[i+1] - a[i];
+		}
+	}
+	double f = a[0], ld = l - a[n-1];
+	double mx = (double)maxd / 2;
+	double ans = fmax(mx, max(f,ld));
+	printf("%.9f", ans);
+	return 0;
+}
