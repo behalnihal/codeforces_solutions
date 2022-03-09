7435d17b6d97b40908b58e350f777f047c6fbc1b
Nihal Gupta <nihalgupta140@gmail.com>
2022-03-09 12:12:00 +0530

    Add solution for problem `B - DIV + MOD` Link: https://codeforces.com/contest/1650/problem/B Tags: math, *900 Ref: https://codeforces.com/contest/1650/submission/148954156

---
+++ codeforces/1650/B.cpp
@@ -0,0 +1,42 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+const int32_t MOD = 1e9 + 7;
+#define ll long long
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(s) s.begin(),s.end()
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+ll expo(ll a,ll b,ll m){a%=m;ll res=1;while(b){if(b&1)res=(a*res)%m;a=(a*a)%m;b>>=1;}return res;}
+
+
+void solve()
+{	 
+	int l,r,a;
+	cin >> l >> r >> a;
+	int ans = r/a + r%a;
+	int x = r - r%a - 1;
+	if(x>=l) ans = max(ans, x/a + x%a);
+	cout << ans << '\n';
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int t = 1;
+	cin >> t;
+
+	while(t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
