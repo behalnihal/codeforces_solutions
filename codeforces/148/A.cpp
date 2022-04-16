a6ed8fe4352c53bde4d7cfaf4eb957ea1b97e91c
Nihal Gupta <nihalgupta140@gmail.com>
2022-04-16 12:58:00 +0530

    Add solution for problem `A - Insomnia cure` Link: https://codeforces.com/contest/148/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/148/submission/153783324

---
+++ codeforces/148/A.cpp
@@ -0,0 +1,49 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+const int MOD = 1000000007;
+#define ll long long
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(s) s.begin(),s.end()
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+#define sz(a) a.size()
+
+ll expo(ll a,ll b,ll m){a%=m;ll res=1;while(b){if(b&1)res=(a*res)%m;a=(a*a)%m;b>>=1;}return res;}
+ll binpow(ll a,ll b){ll res=1;while(b){if(b&1)res=(res*a);a=(a*a);b>>=1;}return res;}
+ll gcd(ll a , ll b){return (b ? gcd(b, a%b) : a);}
+
+int a[10];
+void solve()
+{
+	int k,l,m,n,d;
+	cin >> k >> l >> m >> n >> d;
+	int ans = 0;
+	FOR(i,1,d){
+		if(i%k==0)ans++;
+		else if(i%l==0)ans++;
+		else if(i%m==0)ans++;
+		else if(i%n == 0)ans++;
+	}
+	cout << ans;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int t = 1;
+	// cin >> t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
+
