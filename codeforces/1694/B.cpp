405a5ef7d1631974350d89f6b625e25f5d816666
Nihal Gupta <nihalgupta140@gmail.com>
2022-06-16 21:19:00 +0530

    Add solution for problem `B - Paranoid String` Link: https://codeforces.com/contest/1694/problem/B Tags: constructive algorithms, greedy, *1200 Ref: https://codeforces.com/contest/1694/submission/160866427

---
+++ codeforces/1694/B.cpp
@@ -0,0 +1,82 @@
+// author : nihal_gupta 
+
+#include"bits/stdc++.h"
+#include<chrono>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+typedef long long ll;
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+#define pb push_back
+#define pii pair<int,int>
+#define mp make_pair
+#define F first
+#define S second
+#define double long double
+#define all(s) s.begin(),s.end()
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+#define sz(a) (int)a.size()
+#define min3(a,b,c) min(a, min(b,c))
+#define print(a) for(auto x : a) cout << x << ' '; cout << '\n'
+#define INF 1e18
+
+/**********************************************************************************************/
+
+ll expo(ll a,ll b,ll m){a%=m;ll res=1;while(b){if(b&1)res=(a*res)%m;a=(a*a)%m;b>>=1;}return res;}
+ll binpow(ll a,ll b){ll res=1;while(b){if(b&1)res=(res*a);a=(a*a);b>>=1;}return res;}
+ll gcd(ll a,ll b){return (b ? gcd(b, a%b) : a);}
+ll modsub(ll a,ll b,ll m){return ((a%m - b%m) + m)%m;}
+ll modadd(ll a,ll b,ll m){return (a%m + b%m)%m;}
+ll modmult(ll a, ll b, ll m){return ((a%m) * (b%m))%m;}
+
+/**********************************************************************************************/
+
+void solve()
+{
+	int n;
+	string s;
+	cin >> n >> s;
+	ll ans = n;
+	ll cnt[2] = {0};
+	for(char ch : s){
+		cnt[ch - '0']++;
+	}
+	if(cnt[0] == 0 or cnt[1] == 0){
+		cout << ans << '\n';
+		return;
+	}
+	// ans = (n * 1ll * (n + 1) / 2);
+	// cout << ans << '\n';
+	rep(i,0,n-1){
+		if(s[i] != s[i+1])ans+=i+1;
+	}
+	cout << ans << '\n';
+}
+int main()
+{
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+	auto start = chrono::high_resolution_clock::now();
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t = 1;
+	cin >> t;
+	while(t--){
+		solve();
+	}
+
+	auto end = chrono::high_resolution_clock::now();
+	double time_taken = chrono::duration_cast<chrono::nanoseconds>(end-start).count();
+	time_taken *= 1e-9;
+	// cerr << "Run Time : " << fixed << time_taken << setprecision(9) << " sec";
+
+	return 0;
+}
